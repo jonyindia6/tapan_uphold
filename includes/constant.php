1f5c3764d47d64b9bad06c9ab88549cadddce7ba
vinay sachan <vinay@tappetbox.com>
2024-09-08 22:36:19 +0530

    changes

--- includes/constant.php
+++ includes/constant.php
@@ -1,8 +1,8 @@
 <?php
 define('CONST_MAIL_SERVER', 'smtpout.secureserver.net');
-define('CONST_MAIL_USERNAME', 'contact@livledger.com');
-define('CONST_MAIL_PASSWORD', 'EmailPassword@123');
-define('CONST_MAIL_FROM', 'contact@livledger.com');
+define('CONST_MAIL_USERNAME', 'help@travelbiy.com');
+define('CONST_MAIL_PASSWORD', 'Help@me123');
+define('CONST_MAIL_FROM', 'help@travelbiy.com');
 define('CONST_MAIL_PORT', '465');
 define('CONST_MAIL_FROM_NAME', 'Uphold Contact');
 define('CONST_RECEIVE_EMAIL', 'gracebarrows8722@outlook.com');
